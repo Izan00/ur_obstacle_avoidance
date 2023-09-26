7998e7ee8be445bd25bafa433b9136c62f8345fc
Izan Leal <izanleal@gmail.com>
2023-09-26 23:20:48 +0200

    robot self filter (working) + mesh reconstruct(v0)

--- setup.sh
+++ setup.sh
@@ -6,12 +6,13 @@ sudo apt-get install ros-noetic-realsense2-camera -y
 sudo apt-get install ros-noetic-ur-gazebo ros-noetic-ur3-moveit-config -y
 sudo apt-get install ros-noetic-ros-numpy
 sudo apt-get install ros-noetic-tf2-sensor-msgs
+sudo apt-get install ros-noetic-sensor-filters
 sudo apt-get install python3-pcl -y
 pip install numpy==1.20.3
 pip install -U scikit-learn
 pip install open3d
 pip install pyvista
-
+pip install pymeshfix
 #cd src/
 
 #git clone --recurse-submodules https://github.com/rickstaa/realsense-ros-gazebo.git src
